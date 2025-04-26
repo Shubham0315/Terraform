9cb5965ab6cd8230c532d6b413dacaabfe558611
Shubham Haudge <105341138+Shubham0315@users.noreply.github.com>
2025-04-26 20:00:28 +0530

    Create variables.tf

---
+++ Day3/variables.tf
@@ -0,0 +1,10 @@
+variable "ami_value" {
+    description = "value for the ami"
+    # default = ""
+}
+
+variable "instance_type_value" {
+    description = "value for instance type"
+}
+
+#We can also define variable for subnet
