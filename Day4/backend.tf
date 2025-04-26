eb58908b5aa27b7d3ee0d8d5f9a511a4d0bc4f28
Shubham Haudge <105341138+Shubham0315@users.noreply.github.com>
2025-04-27 00:56:45 +0530

    Update backend.tf

--- Day4/backend.tf
+++ Day4/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "shubham-s3-315"
+    bucket = "shubham-s3-0315"
     region = "us-east-1"
     key = "shubham/terraform.tfstate"
     dynamodb_table = "terraform_lock"
