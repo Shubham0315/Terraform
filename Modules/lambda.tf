81ee25e5bd957027ec0ff63bb24d71f3321cdb75
Shubham Haudge <105341138+Shubham0315@users.noreply.github.com>
2025-08-22 01:36:25 -0500

    Update lambda.tf

--- Modules/lambda.tf
+++ Modules/lambda.tf
@@ -1,7 +1,24 @@
+
+
 module "lambda-function" {
   source = "terraform-aws-modules/lambda/aws"
   version = "~> 6.0"
 
-  
+  function_name = "shubham_lambda"
+  description = "My Lambda"
+  handler = "index.handler"
+  runtime = "python3.9"
+
+  source_path = "./src"
+
+  envoironment_variables = {
+    ENV = "dev"
+  }
+
+  tags = {
+    Owner = "DevOps"
+    Environment = "Dev"
+  }
+}
 
 }
